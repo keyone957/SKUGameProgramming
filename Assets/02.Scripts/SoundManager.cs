3e36a2247e543f287f7824e9b2aded8ccbc61156
Hone Won Gi <keyone957@naver.com>
2024-05-01 22:21:43 +0900

    Feat : 플레이어 state적용(FSM),플레이어 따라다니는 캠
    - 플레이어 동작에 따라 애니메이션 동작
    - 플레이어 따라다니는 카메라 추가
    - 타일맵, 이펙트 추가

--- Assets/02.Scripts/SoundManager.cs
+++ Assets/02.Scripts/SoundManager.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 //게임 내 bgm 및 효과음 플레이 및 관리 하기 위한 SoundManager
 // 최초 작성자 : 홍원기
-// 수정자 : 
-// 최종 수정일 : 2024-04-05
+// 수정자 : 홍원기
+// 최종 수정일 : 2024-04-12
 public class SoundManager : MonoBehaviour
 {
     [SerializeField] private AudioSource _bgmSource = null;
@@ -23,6 +23,7 @@ public class SoundManager : MonoBehaviour
         if (_instance == null)
         {
             _instance = this;
+            //효과음과, 배경음악은 어느 씬에서나 사용하기 때문에 DonDestroyOnLoad를 이용하여 삭제가 되지 않게
             DontDestroyOnLoad(this.gameObject);
         }
         else
@@ -53,6 +54,7 @@ public class SoundManager : MonoBehaviour
     }
 
     #region BGM
+    
 
     private static string GetBGMFullPath(string path) => Define._bgmRoot + "/" + path;
     public void LoadBGM(string path) => LoadAudioClip(GetBGMFullPath(path));
