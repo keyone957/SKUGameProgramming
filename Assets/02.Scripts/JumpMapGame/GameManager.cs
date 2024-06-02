8a50fb3bcf121135abea03c90631ed3c043c8fae
bokobok <jskjuj0223@gmail.com>
2024-06-02 17:06:42 +0900

    Feat:게임 매니저 컴포넌트 작성
    - gamemanager 컴포넌트 작성
    - 팝업창 생성
    - 스타트 버튼 생성
    - 게임 설명 텍스트 추가
    - 3초 카운트다운 텍스트 작성
    -  ui들 폰트 변경

--- Assets/02.Scripts/JumpMapGame/GameManager.cs
+++ Assets/02.Scripts/JumpMapGame/GameManager.cs
@@ -1,18 +1,58 @@
 using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+public class GameStartManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject popupPanel;  // Panel 오브젝트
+    public TextMeshProUGUI countdownText;  // 카운트다운 텍스트
+    public Button startButton;  // Start 버튼
+    public TextMeshProUGUI gameDescriptionText;  // 게임 설명 텍스트
+
+    private bool gameStarted = false;  // 게임이 시작되었는지 여부를 나타내는 플래그
+
     void Start()
     {
-        
+        // 항상 초기 UI 설정 실행
+        countdownText.gameObject.SetActive(false);  // 카운트다운 텍스트 비활성화
+        popupPanel.SetActive(true);  // 팝업창을 활성화
+        startButton.onClick.AddListener(OnStartButtonClicked);  // Start 버튼 클릭 이벤트 등록
+        Debug.Log("start");
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnStartButtonClicked()
     {
-        
+        if (!gameStarted)  // 게임이 시작되지 않았을 때만 실행
+        {
+            StartCoroutine(StartCountdown());  // 카운트다운 시작
+        }
+    }
+
+    IEnumerator StartCountdown()
+    {
+        popupPanel.SetActive(false);  // 팝업창을 비활성화
+        countdownText.gameObject.SetActive(true);  // 카운트다운 텍스트 활성화
+
+        for (int i = 3; i > 0; i--)
+        {
+            countdownText.text = i.ToString();  // 카운트다운 숫자 표시
+            yield return new WaitForSeconds(1);  // 1초 대기
+        }
+
+        countdownText.text = "Start!!";  // 카운트다운 완료 메시지
+        yield return new WaitForSeconds(1);  // 1초 대기
+
+        countdownText.gameObject.SetActive(false);  // 카운트다운 텍스트 비활성화
+
+        // 게임 시작 로직 추가
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        gameStarted = true;  // 게임이 시작되었음을 표시
+        SceneManager.LoadScene("JumpMapBonus");
     }
 }
