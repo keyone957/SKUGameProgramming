69e80c139b84a9bec18248ab90a2d243cf206d12
april0114 <sue5520607@skuniv.ac.kr>
2024-06-08 10:42:24 +0900

    Feat : 원에 닿게 될 시 코인 활성화

---
+++ Assets/02.Scripts/CoinBonusMap/CoinManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// 코인 전체 관리 
+// 최초 작성자: 하경림
+// 수정자: 하경림
+// 최종 수정일: 2024-06-06
+public class CoinManager : MonoBehaviour
+{
+    public List<GameObject> coins = new List<GameObject>();
+
+    void Start()
+    {
+        DeactivateAllCoins();
+    }
+
+    public void ActivateCoin(GameObject coin)
+    {
+        coin.SetActive(true);
+    }
+
+    public void ActivateAllCoins()
+    {
+        foreach (GameObject coin in coins)
+        {
+            coin.SetActive(true);
+        }
+    }
+
+    public void DeactivateAllCoins()
+    {
+        foreach (GameObject coin in coins)
+        {
+            coin.SetActive(false);
+        }
+    }
+}
--- Assets/02.Scripts/CoinBonusMap/CoinTrigger.cs
+++ Assets/02.Scripts/CoinBonusMap/CoinTrigger.cs
@@ -1,36 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
-
-// 원 닿게 될시, 코인 생성 및 사라지는 트리거
+// 코인 닿게 될시
 // 최초 작성자: 하경림
 // 수정자: 하경림
 // 최종 수정일: 2024-06-06
-
-public class CircleTrigger : MonoBehaviour
-{
-    private bool collided = false;
+public class CoinTrigger : MonoBehaviour
+{ private bool collided = false;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player") && !collided)
         {
             collided = true;
-            Destroy(gameObject);
 
             CoinManager coinManager = FindObjectOfType<CoinManager>();
             if (coinManager != null)
             {
                 coinManager.ActivateAllCoins();
             }
-            
 
-            CoinTimerScript timerComponent = FindObjectOfType<CoinTimerScript>();
-            if (timerComponent != null)
-            {
-                timerComponent.StartCountdown();
-            }
         }
     }
 }
